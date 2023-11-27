b153803b6d962b1c36f4e37230bafb7448073fee
Ayush Jha <ayushjha_ep20a5_45@dtu.ac.in>
2023-11-27 14:54:49 +0530

    fixed port

--- index.js
+++ index.js
@@ -124,12 +124,7 @@ app.get('/api/globalCityStructure', async (req, res) => {
 }
 );
 
-const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-  
-});
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
