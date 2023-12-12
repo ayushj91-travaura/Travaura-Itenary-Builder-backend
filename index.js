078c7a0592d76694879c69f3a97346cd2a5a0096
Ayush Jha <ayushjha_ep20a5_45@dtu.ac.in>
2023-12-12 13:15:10 +0530

    added puppetteer

--- index.js
+++ index.js
@@ -9,9 +9,12 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 // app.use(express.json());
 
+
+
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+app.use(bodyParser.json({ limit: '100mb' })); 
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
 
 
 
