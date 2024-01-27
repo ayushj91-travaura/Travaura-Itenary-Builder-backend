83f37bc56afd282f953c91bd624aeba546a2401e
Ayush Jha <ayushjha@travaura.com>
2024-01-27 10:38:02 +0530

    new updates

--- index.js
+++ index.js
@@ -339,6 +339,7 @@ app.post('/api/user', async (req, res) => {
       userdata = new user({
         _id: req.body.id,
         agentEmail: req.body.agentEmail,
+        agentUID: req.body.agentUID,
               travellerDetails: req.body.travellerDetails,
               country: req.body.country,
               selectedActivities: req.body.selectedActivities,
